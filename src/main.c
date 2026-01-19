83eedd22da82013b02d3d3a7dd95c0d1a58a9609
nyoravim <nora@bedafamily.com>
2026-01-18 20:50:16 -0800

    cleaning up logging

--- src/main.c
+++ src/main.c
@@ -43,31 +43,43 @@ enum {
 static struct dataset* load_dataset_by_id(uint32_t id) {
     const char* labels;
     const char* images;
+    const char* name;
 
     switch (id) {
     case DATASET_TRAINING:
         labels = "data/train-labels-idx1-ubyte.gz";
         images = "data/train-images-idx3-ubyte.gz";
+        name = "training";
 
-        log_info("loading training dataset");
         break;
     case DATASET_TESTING:
         labels = "data/t10k-labels-idx1-ubyte.gz";
         images = "data/t10k-images-idx3-ubyte.gz";
+        name = "testing";
 
-        log_info("loading testing dataset");
         break;
     default:
         log_warn("invalid dataset: %u", id);
         return NULL;
     }
 
-    return dataset_load(labels, images);
+    log_debug("loading %s dataset", name);
+
+    struct dataset* data = dataset_load(labels, images);
+    if (!data) {
+        log_error("failed to load %s dataset!", name);
+        return NULL;
+    }
+
+    log_info("loaded %s dataset", name);
+    return data;
 }
 
 static void free_dataset(void* user, void* value) { dataset_free(value); }
 
 static nv_map_t* load_datasets() {
+    log_trace("loading datasets");
+
     struct nv_map_callbacks callbacks;
     memset(&callbacks, 0, sizeof(struct nv_map_callbacks));
 
@@ -79,7 +91,6 @@ static nv_map_t* load_datasets() {
     for (uint32_t id = 0; id < DATASET_COUNT; id++) {
         struct dataset* data = load_dataset_by_id(id);
         if (!data) {
-            log_error("failed to load dataset id %u!", id);
             continue;
         }
 
@@ -90,8 +101,6 @@ static nv_map_t* load_datasets() {
 }
 
 int main(int argc, const char** argv) {
-    log_info("loading datasets");
-
     nv_map_t* datasets = load_datasets();
     if (nv_map_size(datasets) < DATASET_COUNT) {
         nv_map_free(datasets);
